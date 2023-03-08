51ba02ba9795c4182f87b062799a584fefcf8b4f
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-03-08 10:45:52 -0600

    Mejora de diseño, se agregaron sliders

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,13 @@
 <script>
 import boton from '../components/buttsSubmit.vue'
+import slider from '../components/InputSlider.vue';
 
 import axios from 'axios';
 export default {
   components:{
-    boton
+    boton,slider
   },
+  emits: ['person'],
     data() {
         return {
             Nombre: " ",
@@ -16,10 +18,10 @@ export default {
             Cita: " ",
             CitaAutor: " ",
             Bio: " ",
-            Personalidad1: " ",
-            Personalidad2:" ",
-            Personalidad3: " ",
-            Personalidad4: " ",
+            Personalidad1: 50,
+            Personalidad2: 50,
+            Personalidad3: 50,
+            Personalidad4: 50,
             Objetivos: " ",
             Frustraciones: [],
             Motivaciones: [],
@@ -72,7 +74,7 @@ export default {
   <h1 class="my-5 text-xl">Llena el Formulario de sus datos personales</h1>
   <div id="app">
   <form>
-    <div class="pl-5 grid gap-5  grid-cols-3 bg-blue-300">
+    <div class="pl-5 grid gap-5  grid-cols-3 bg-blue-300 text-center">
     <div class="my-5">
     <label for="nombre" class="text-lg">Nombre</label><br>
     <input type="text" id="nombre" v-model="Nombre" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingresa nombre">
@@ -99,54 +101,43 @@ export default {
     </div>
     </div>
     <h1 class="my-5 text-xl">Datos de personalidad</h1>
-    <div class="pl-5 grid gap-6 grid-cols-3 bg-blue-300">
-    <div class="my-5">
-    <label for="cita" class="text-md">Escribe una cita</label><br>
+    <div class="pl-5 bg-blue-300 text-center my-10 grid grid-cols-3 gap-4">    
+    <div class="my-10">
+    <label for="cita" class="text-lg">Escribe una cita</label><br>
     <input type="text" id="cita" v-model="Cita" class="form-input border-gray-300 p-2 rounded-md text-black h-full w-full" placeholder="Ingrese una cita">
-    </div>
-    <div class="my-5">
     <label for="citaAutor" class="text-lg">Autor de la cita</label><br>
     <input type="text" id="citaAutor" v-model="CitaAutor" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese autor">
     </div>
-    <div class="my-5">
+    <div class="my-10">
     <label for="bio" class="text-lg">Escribe tu bio</label><br>
     <input type="text" id="bio" v-model="Bio" class="form-input border-gray-300 p-2 rounded-md text-black h-full w-full" placeholder="Ingrese tu bio">
     </div>
-    <div class="my-5">
-    <label for="personalidad01" class="text-lg">Porcentaje de personaliad 1</label><br>
-    <input type="int" id="personalidad01" v-model="Personalidad1" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese porcentaje">
+
+    <div class="my-10">
+    <slider @person="pers1" v-model="Personalidad1">Personalidad 01</slider>
+    <br>
+    <slider @person="pers2" v-model="Personalidad2">Personalidad 02</slider>
+    <br>
+    <slider @person="pers3" v-model="Personalidad3">Personalidad 03</slider>
+    <br>
+    <slider @person="pers4" v-model="Personalidad4">Personalidad 02</slider>
     </div>
-    <div class="my-5">
-    <label for="personalidad02" class="text-lg">Porcentaje de personaliad 2</label><br>
-    <input type="int" id="personalidad02" v-model="Personalidad2" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese porcentaje">
-    </div>
-    <div class="my-5">
-    <label for="personalidad03" class="text-lg">Porcentaje de personaliad 3</label><br>
-    <input type="int" id="personalidad03" v-model="Personalidad3" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese porcentaje">
-    </div>
-    <div class="my-5">
-    <label for="personalidad04" class="text-lg">Porcentaje de personaliad 4</label><br>
-    <input type="int" id="personalidad04" v-model="Personalidad4" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese porcentaje">
-    </div>
-    <div class="my-5">
+    <div class="mt-10">
+    <div class="mt-10 grid grid-cols-3 gap-4">
     <label for="Objetivos" class="text-lg">Objetivos</label><br>
     <input type="text" id="Objetivos" v-model="Objetivos" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese Objetivos">
-    </div>
-    <div class="my-5">
     <label for="Frustraciones" class="text-lg">Frustraciones</label><br>
-    <input type="texr" id="Frustaciones" v-model="Frustraciones" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese Frustraciones">
-    </div>
-    <div class="my-5">
+    <input type="text" id="Frustaciones" v-model="Frustraciones" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese Frustraciones">
     <label for="Motivaciones" class="text-lg">Motivaciones</label><br>
     <input type="text" id="Motivaciones" v-model="Motivaciones" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese Motivaciones">
-    </div>
-    <div class="my-5">
     <label for="Marcas" class="text-lg">Marcas</label><br>
     <input type="text" id="Marcas" v-model="Marcas" class="form-input border-gray-300 p-2 rounded-md text-black" placeholder="Ingrese sus marcas">
     </div>
+  </div>
+    
     </div>
 
-    <div>
+    <div class="">
     <boton :desactivar="true" @click="Enviar()">Registrar</boton>
     </div>
     
--- src/components/buttsSubmit.vue
+++ src/components/buttsSubmit.vue
@@ -6,7 +6,7 @@ export default{
 </script>
 <template>
 
-<button :disabled="desactivar" class="bg-green-500 disabled:bg-red-500 rounded-lg p-3">
+<button class="bg-green-500 disabled:bg-red-500 rounded-lg p-3 my-10">
 
     <slot></slot>
 </button>
