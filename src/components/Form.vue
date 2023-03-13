ca9c9ca56bb6a241258982e3e5a3533fdc878bec
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-03-12 23:07:49 -0600

    Agregando mensajes

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -127,18 +127,21 @@ this.Bio=s;
 </script>
 
 <template>
-  <body class="bg-white font-mono flex xl:font-bold">
+  <body class="bg-white font-mono flex justify-center xl:font-bold">
   <div class="p-5 mx-5 bg-white">
   <div id="app">
   <form>
       <h1 class="my-5 text-xl p-5">Llena el Formulario de sus datos personales</h1>
-    <div class="pl-5 grid gap-5 grid-cols-3 bg-blue-300 text-center max-lg:grid-cols-1">  
+    <div class="pl-5 grid gap-5 grid-cols-3 bg-blue-300 text-center rounded-xl max-lg:grid-cols-1">  
     <div class="my-5">
     <label for="nombre" class="text-lg">Nombre</label><br>
+    <p v-if="Nombre == '' " class="text-red-800 text-lg"> Ingresa su nombre, plis :(</p>
     <caja @data="nombreyapellido"></caja>
     </div>
     <div class="my-5">
     <label for="edad" class="text-lg">Edad</label><br>
+    <p v-if="Edad >=100" class="text-red-800"> la edad es invalida</p>
+    <p v-if="Edad == '' " class="text-red-800"> Ingrese su edad, plis :(</p>
     <caja @data="viejo"></caja>
     </div>
     <div class="my-5">
@@ -147,25 +150,29 @@ this.Bio=s;
     </div>
     <div class="my-5">
     <label for="trabajo" class="text-lg">Trabajo</label><br>
+    <p v-if="Trabajo == '' " class="text-red-800"> Ingrese su trabajo, plis :(</p> 
     <caja @data="chamba"></caja>
     </div>
     <div class="my-5">
     <label for="Residencia" class="text-lg">Residencia</label><br>
+    <p v-if="Residencia == '' " class="text-red-800"> Ingrese su pueblito, plis :(</p>
     <caja @data="pueblito"></caja>
     </div>
     </div>
     <h1 class="my-5 text-xl p-5">Datos de personalidad</h1>
-    <div class="pl-5 bg-blue-300 text-center my-10 grid grid-cols-3 gap-4 max-lg:grid-cols-1">    
+    <div class="pl-5 bg-blue-300 text-center my-10 grid grid-cols-3 gap-4 rounded-xl max-lg:grid-cols-1">    
     <div class="my-10">
     <label for="cita" class="text-lg">Escribe una cita</label><br>
-
+    <p v-if="Cita == '' " class="text-red-800"> Ingrese una cita, plis :(</p>
     <cajagrande @testa="inspira"></cajagrande>
 
     <label for="citaAutor" class="text-lg">Autor de la cita</label><br>
+    <p v-if="CitaAutor == '' " class="text-red-800"> Ingrese el autor, plis</p>
     <caja @data="creador"></caja>
     </div>
     <div class="my-10">
     <label for="bio" class="text-lg">Escribe tu bio</label><br>
+    <p v-if="Bio == '' " class="text-red-800"> Ingrese su bio, plis :(</p>
     <cajagrande @testa="biografia"></cajagrande>
     </div>
 
@@ -178,28 +185,27 @@ this.Bio=s;
     <br>
     <slider @person="perso4">Personalidad 02</slider>
     </div>
-
+  </div>
 
     <div class="mt-10">
       <div>
       <h1 class="my-5 text-xl">Datos de persona</h1>
-      <div class="mt-10 grid grid-cols-3 gap-4">
-      <div>
-<div>
+      <div class="mt-10 grid grid-cols-4 gap-5 bg-blue-300 rounded-xl max-lg:grid-cols-1">
+<div class="mx-10 p-5">
                 <label for="objetivos" class="text-xl"> Objetivos: </label> 
                 <div v-for="(obj, index) in Objetivos">
                   <objetivo @objetivo="masobjetivos" :index="index"> </objetivo>
      </div>
      <button v-on:click.prevent="this.Objetivos.push(nuevosobjetivos)" class="px-4 py-2 mb-2 bg-gray-600 text-white rounded"> Agregar</button>
 </div>
-<div>
+<div class="mx-10 p-5">
                <label for="frustraciones" class="text-xl"> Frustraciones: </label> 
                 <div v-for="(obj, index) in Frustraciones">
                   <frustra @frustracion="masfrustraciones" :index="index"></frustra>
                 </div>
                 <button v-on:click.prevent="this.Frustraciones.push(nuevasfrustraciones)" class="px-4 py-2 mb-2 bg-gray-600 text-white rounded"> Agregar</button> 
             </div>
-<div>
+<div class="mx-10 p-5">
                 <label for="motivaciones" class="text-xl"> Motivaciones: </label> 
                 <div v-for="(obj, index) in Motivaciones">
                   <motivacion @motivaciones="masmotivos" :index="index"></motivacion>
@@ -208,16 +214,16 @@ this.Bio=s;
                 <button v-on:click.prevent="this.Motivaciones.push(nuevasmotivaciones)" class="px-4 py-2 mb-2 bg-gray-600 text-white rounded"> Agregar</button>
               </div>
 
-<div>
-    <label for="Marcas" class="text-lg">Marcas</label><br>
+<div class="mx-10 p-5">
+    <label for="Marcas" class="text-xl">Marcas</label><br>
+    <p v-if="marca == '' " class="text-red-800"> Ingrese su marca, plis :(</p>
     <caja @data="marca"></caja>
 </div>
     </div>
 </div>
     </div>
-  </div>
+
     
-    </div>
 
     <div class="flex justify-center">
     <boton @click="Enviar()" type="button">Registrar</boton>
